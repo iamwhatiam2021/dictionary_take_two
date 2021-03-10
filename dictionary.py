7dfe148d1cc8c78913dd2c3fa694de642e7f6d31
JS <jsedrak@student.ie.edu>
2021-03-10 17:51:07 -0500

    Dictionary Assignment

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+
+
+#english = {"Hello", "Goodbye", "Good Morning", "Good Evening", "Good Night", "Welcome", "How are you?", "How\'s it going?", "How\'ve you been?", "I\'m fine thank you", "Thank you", "Please", "You\'re welcome", "Excuse me", "Sorry", "Have fun", "Well done!", "Take care!", "See you later", "See you soon", "See you tomorrow", "See you"}
+#spanish = ("Hola", "Adios", "Buenos dias", "Buenas tardes", "Buenas noches", "Bienvenido", "Como Estas?", "Como Te Va?", "Como te ha ido?", "Estoy bien Gracias", "Gracias", "Por favor", "De nada!", "Perdone!", "Lo siento!", "Diviertete", "Muy bien!", "Cuidate!", "Hasta luego!", "Hasta pronto!", "Hasta manana!", "Nos vemos")
+
+language = {"Hello":"Hola", "Goodbye": "Adios", "Good Morning": "Buenos dias", "Good Evening": "Buenas tardes", "Good Night": "Buenas noches", "Welcome":  "Bienvenido", "How are you?": "Como Estas?", "How\'s it going?": "Como Te Va?", "How\'ve you been?": "Como te ha ido?", "I\'m fine thank you": "Estoy bien Gracias", "Thank you": "Gracias", "Please": "Por favor", "You\'re welcome": "De nada!", "Excuse me": "Perdone!"}
+
+#print(language[''])
